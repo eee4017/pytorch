e8c6e007a4834417c38a8cddbcd840f2efa476b3
Hui Guo <huiguo@fb.com>
2021-09-01 16:12:30 -0700

    [tensorexpr] Add an optional argument for set of unallocated intermediate buffers in prepareForCodegen/insertAllocFree
    ghstack-source-id: 8c75f53b333d9bd6811952bb5d74e15e55fbbb15
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/64226

--- torch/csrc/jit/tensorexpr/loopnest.cpp
+++ torch/csrc/jit/tensorexpr/loopnest.cpp
@@ -59,7 +59,7 @@ LoopNest::LoopNest(const std::vector<Tensor>& output_tensors) {
   verify(root_stmt_);
 }
 
-const std::unordered_set<BufPtr> LoopNest::getIntermediateBufs() const {
+std::unordered_set<BufPtr> LoopNest::getIntermediateBufs() const {
   std::unordered_set<BufPtr> result;
   auto input_bufs = getInputBufs();
   auto bufs = NodeFinder<Buf>::find(root_stmt_);
@@ -946,8 +946,17 @@ BlockPtr findLowestContainingBlock(const std::vector<BufLoadOrStoreUse>& uses) {
   return b;
 }
 
-StmtPtr LoopNest::insertAllocFree(StmtPtr stmt) {
-  auto intermediate_bufs = getIntermediateBufs();
+StmtPtr LoopNest::insertAllocFree(
+    StmtPtr stmt,
+    const c10::optional<std::unordered_set<BufPtr>>&
+        interm_bufs /* = c10::nullopt*/) {
+  std::unordered_set<BufPtr> intermediate_bufs;
+  if (interm_bufs) {
+    intermediate_bufs = *interm_bufs;
+  } else {
+    intermediate_bufs = getIntermediateBufs();
+  }
+
   if (intermediate_bufs.size() == 0ULL) {
     return stmt;
   }
@@ -961,6 +970,9 @@ StmtPtr LoopNest::insertAllocFree(StmtPtr stmt) {
       findLoadOrStoreUses(stmt);
   // Insert allocations and frees for temporary buffers at global scope.
   for (BufPtr buf : intermediate_bufs) {
+    if (buf->is_allocated()) {
+      continue;
+    }
     b->prepend_stmt(alloc<Allocate>(buf));
     b->append_stmt(alloc<Free>(buf));
   }
@@ -1024,7 +1036,9 @@ void LoopNest::eliminateDeadStores() {
   root_stmt_ = root_stmt_->accept_mutator(&deleter);
 }
 
-void LoopNest::prepareForCodegen() {
+void LoopNest::prepareForCodegen(
+    const c10::optional<std::unordered_set<BufPtr>>&
+        interm_bufs /*= c10::nullopt*/) {
   // Expand reduction ops.
   ReductionExpander reduceExpander;
   root_stmt_ = reduceExpander.expand(root_stmt_);
@@ -1032,7 +1046,7 @@ void LoopNest::prepareForCodegen() {
   root_stmt_ = FlattenIndexes(root_stmt_);
 
   // Add allocs and frees for intermediate buffers at the global level.
-  root_stmt_ = insertAllocFree(root_stmt_);
+  root_stmt_ = insertAllocFree(root_stmt_, interm_bufs);
 }
 
 namespace {
