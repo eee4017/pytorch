6af6c5ce26b9ac3dfd4032827c5d8969c9fb2f2d
Frank Lin <eee4017@gmail.com>
2026-03-18 17:35:16 +0000

    stateless-rng-by-jbschlosser

--- torchgen/native_function_generation.py
+++ torchgen/native_function_generation.py
@@ -51,6 +51,9 @@ MUTABLE_OPS_THAT_CANNOT_GET_AN_OUT_VARIANT = [
     "_cummax_helper",
     # should be out=?
     "_cummin_helper",
+    # stateless RNG ops that write directly into self
+    "_philox_normal_",
+    "_philox_uniform_",
 ]
 
 # All of these operators don't have any tensor like returns
